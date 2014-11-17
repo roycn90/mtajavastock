1b361f02fa96b9d0c385316bd46114806186c452
Roi <Roi@Roi-PC>
2014-11-17 16:53:44 +0200

    coomitment2

--- src/com/roy/RoyAppServlet.java
+++ src/com/roy/RoyAppServlet.java
@@ -25,14 +25,22 @@ public class RoyAppServlet extends HttpServlet {
 		String result2 = "<h1>Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is  cm.  ";
 		
 	*/
+		double opposite;
+		int hypotenuse=50;
+		int angleDeg=30;
 		
+		opposite = Math.sin(Math.toRadians(angleDeg)) * hypotenuse ;
+		String result2 = new String("<h1>Length of opposite where angle B is 30 degrees and Hypotenuse length is 50 cm is "+(int)Math.round(opposite)+" cm</h1>") ;
+		
+
+			
 	
 		double res3= pow (20,13);
 		String result3 = "<h1>power of 20 with exp of 13 is "+res3+".  ";
 	
 		resp.setContentType("text/html");
 		resp.getWriter().println(result1);
-		/*resp.getWriter().println(result2); */
+		resp.getWriter().println(result2);
 		resp.getWriter().println(result3);
 	}
 
